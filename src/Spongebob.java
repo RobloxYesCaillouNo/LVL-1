915f48fc365c8b2804ead9083250b1e4aa357871
RobloxYesCaillouNo <league@WTS-IM-17.attlocal.net>
2017-11-04 16:01:01 -0700

    stuff
    iyeuhjhhihik

---
+++ src/Spongebob.java
@@ -0,0 +1,13 @@
+
+public class Spongebob {
+public static void main(String[] args) {
+	SeaCreature spongebob = new SeaCreature("spongebob");
+spongebob.eat();
+spongebob.laugh();
+SeaCreature patrick = new SeaCreature("patrick");
+
+System.out.println(patrick.getName());
+
+
+}
+}
