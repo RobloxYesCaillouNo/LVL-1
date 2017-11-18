35fce63cb86d183f0b84769438cadf54ba2b1f80
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2017-11-18 15:58:25 -0800

    vbvbvbggfvffgg

--- src/Spongebob.java
+++ src/Spongebob.java
@@ -1,13 +1,12 @@
 
 public class Spongebob {
-public static void main(String[] args) {
-	SeaCreature spongebob = new SeaCreature("spongebob");
-spongebob.eat();
-spongebob.laugh();
-SeaCreature patrick = new SeaCreature("patrick");
+	public static void main(String[] args) {
+		SeaCreature spongebob = new SeaCreature("spongebob");
+		spongebob.eat();
+		spongebob.laugh();
+		SeaCreature patrick = new SeaCreature("patrick");
 
-System.out.println(patrick.getName());
+		System.out.println(patrick.getName());
 
-
-}
+	}
 }
