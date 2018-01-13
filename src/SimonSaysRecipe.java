470202593f7d7ab75b58c6a0a292d4888807d873
Jayden Kudo <league@WTS19.attlocal.net>
2018-01-13 15:58:49 -0800

    lIlIlIlIlI noice
    no need to shout no need to yell no need to have a riot. Shut ur eyes
    take a deep breath ahh u got all quiet

--- src/SimonSaysRecipe.java
+++ src/SimonSaysRecipe.java
@@ -11,85 +11,121 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import java.io.IOException;
+
 public class SimonSaysRecipe extends KeyAdapter {
- 
- // Complete steps 1 - 7 before you test
- // 1. Make a JFrame variable 
- JFrame frame = new JFrame();
- 	HashMap<Integer,String> images = new HashMap<Integer, String>();
- 	private int imageIndex;
- 	private int tries = 0;
- 	private int simonSays = 0;
- 	Date timeAtStart;
- 	private  void makeAlbum() {
- // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
-  	images.put(KeyEvent.VK_UP, "image.jpg");
- // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
- 
- // 4. call the method to show an image
 
- 	}
+	// Complete steps 1 - 7 before you test
+	// 1. Make a JFrame variable
+	JFrame frame = new JFrame();
+	HashMap<Integer, String> images = new HashMap<Integer, String>();
+	private int imageIndex;
+	private int tries = 0;
+	private int simonSays = 0;
+	Date timeAtStart;
+
+	private void makeAlbum() {
+		// 2. add 4 images which match keyboard keys like this: images.put(new
+		// Integer(KeyEvent.VK_UP), "image.jpg");
+		images.put(KeyEvent.VK_UP, "up.png");
+		images.put(KeyEvent.VK_DOWN, "down.png");
+		images.put(KeyEvent.VK_LEFT, "left.png");
+		images.put(KeyEvent.VK_RIGHT, "right.png");
+		// 3. Tell the user to "Press the matching key when 'Simon says' otherwise press
+		// a different key"
+		JOptionPane.showMessageDialog(null, "Press the key when simon says otherwise press a different key");
+		// 4. call the method to show an image
+		showImage();
+	}
+int points = 0;
 	public void keyPressed(KeyEvent e) {
-    	int keyCode = e.getKeyCode();
-    	// 16. make a points variable to track the score. tell the user their score at the end.
-    	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
-    	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
-    	//19. Use the speak method to tell the user if they were correct or not
-    	//13. increment tries by 1
-  	
-    	//14. if tries is greater than 9 (or however many you want)
-    	
-    	//15.    	exit the program
-
-    	//11. dispose of the frame
-   	
-    	//12. call the method to show an image
-
+		int keyCode = e.getKeyCode();
+		// 16. make a points variable to track the score. tell the user their score at
+		// the end.
+		
+		
+		// 17. if the keyCode matches the imageIndex and "Simon says..." increase their
+		// score
+		if (keyCode == imageIndex && simonSays == 1)  {
+			points += 1;
+		speak("You are correct");
+		}
+		// 18. if the keyCode doesn't match the imageIndex and "Simon didn't say..."
+		// increase their score
+		else if (keyCode != imageIndex && simonSays == 0) {
+			points += 1;
+			speak("You are correct");
+		}
+		else {
+			speak("Get rekt nub, u haz faild! You are incorrect");
+		}
+		JOptionPane.showMessageDialog(null, "Your score is " +points+ "!");
+		// 19. Use the speak method to tell the user if they were correct or not
+		// 13. increment tries by 1
+		tries += 1;
+		// 14. if tries is greater than 9 (or however many you want)
+		if(tries > 9) {
+			// 15. exit the program
+			System.exit(0);
+		}
+		// 11. dispose of the frame
+		frame.dispose();
+		// 12. call the method to show an image
+		showImage();
 	}
+
 	private void showImage() {
-    	//5. initialize your frame to a new JFrame()
-	 
-    	//6. set the frame to visible
+		// 5. initialize your frame to a new JFrame()
+		JFrame framed = new JFrame();
+		// 6. set the frame to visible
+		framed.setVisible(true);
+		// frame.add(getNextRandomImage()); //7. rename to the name of your frame
+		framed.add(getNextRandomImage());
+		// 8. set the size of the frame
+		framed.setSize(2000, 1000);
+		// 9. add a key listener to the frame
+		framed.addKeyListener(this);
+		// 10. Use the speak method to either say "Simon says press this key" or "Press
+		// this key"
+		simonSays = new Random().nextInt(2);
+		// Hint: use the simonSays int and a random number
+		if(simonSays == 1) {
+		speak("simonSays press this key!");
+		}
+		else {
+			speak("Press this key");
+		}
+}
 	
-  	 //frame.add(getNextRandomImage()); //7. rename to the name of your frame
-    	
-    	// 8. set the size of the frame 
-     	
-    	// 9. add a key listener to the frame
-	
-   	 //10. Use the speak method to either say "Simon says press this key" or "Press this key"
-    	//Hint: use the simonSays int and a random number
-	}
+
 	private Component getNextRandomImage() {
-    	this.imageIndex = new Random().nextInt(4) + 37;
-    	return loadImage(images.get(imageIndex));
+		this.imageIndex = new Random().nextInt(4) + 37;
+		return loadImage(images.get(imageIndex));
 	}
+
 	private JLabel loadImage(String fileName) {
-    	URL imageURL = getClass().getResource(fileName);
-    	Icon icon = new ImageIcon(imageURL);
-    	return new JLabel(icon);
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
 	}
-  void speak(String words) {
-  	  try {
-  	   Runtime.getRuntime().exec("say " + words).waitFor();
-  	  } catch (Exception e) {
-  	   e.printStackTrace();
-  	  }
-   	}
+
+	void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
-	 new SimonSaysRecipe().makeAlbum();
+		new SimonSaysRecipe().makeAlbum();
 	}
 }
 
-/* 
-* 20. add a timer
-* ~~~ where the code starts running ~~~
-* timeAtStart = new Date();
-*
-* ~~~ where the code ends ~~~
-* Date timeAtEnd = new Date();
-* System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
-* System.exit(0);
-*/
-
-
+/*
+ * 20. add a timer ~~~ where the code starts running ~~~ timeAtStart = new
+ * Date();
+ *
+ * ~~~ where the code ends ~~~ Date timeAtEnd = new Date();
+ * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
+ * System.exit(0);
+ */
